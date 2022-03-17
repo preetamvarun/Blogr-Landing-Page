256e466a0da020f4d7c669d596da3a1a001281d5
Preetam Varun <preetamarun99@gmail.com>
2022-03-17 21:44:25 +0530

    now I have to style the website for mobile mode

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ const ans3 = document.getElementById('ans-box-3');
 
 arrow1.addEventListener('click', () => {
     arrow1.classList.toggle("inverse");
-    arrow1.classList.contains("inverse") ? ans1.style.opacity = 1 : ans1.style.opacity = 0;
+    arrow1.classList.contains("inverse") ? ans1.style.display = 'block' : ans1.style.display = 'none';
 });
 
 arrow2.addEventListener('click',() => {
     arrow2.classList.toggle("inverse");
-    arrow2.classList.contains("inverse") ? ans2.style.opacity = 1 : ans2.style.opacity = 0;
+    arrow2.classList.contains("inverse") ? ans2.style.display = 'block' : ans2.style.display = 'none';
 });
 
 arrow3.addEventListener('click',() => {
     arrow3.classList.toggle("inverse");
-    arrow3.classList.contains("inverse") ? ans3.style.opacity = 1 : ans3.style.opacity = 0;
+    arrow3.classList.contains("inverse") ? ans3.style.display = 'block' : ans3.style.display = 'none'; 
 });
